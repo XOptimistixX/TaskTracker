b9900ef138fe44999b655ff2814ef38b1f04a990
daniellim <daniellimweesoong2000@gmail.com>
2016-07-12 00:35:24 +0800

    Finally can push liao. With the channels thingy and fixed silent login problem.

---
+++ app/src/main/java/com/jeffsieu/tasktracker/Channel.java
@@ -0,0 +1,35 @@
+package com.jeffsieu.tasktracker;
+
+import java.util.ArrayList;
+
+/**
+ * Created by user on 10/7/2016.
+ */
+public class Channel {
+    private String name;
+    private String code;
+    private ArrayList<String> admins;
+
+    public Channel(String name, String code, String uid) {
+        this.name = name;
+        this.code = code;
+        admins = new ArrayList<>();
+        admins.add(uid);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
--- app/src/main/java/com/jeffsieu/tasktracker/activity/MainActivity.java
+++ app/src/main/java/com/jeffsieu/tasktracker/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.jeffsieu.tasktracker.activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Typeface;
@@ -10,12 +11,15 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.SubMenu;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.gms.auth.api.Auth;
@@ -28,6 +32,8 @@ import com.google.android.gms.common.api.ResultCallback;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
+import com.jeffsieu.tasktracker.Channel;
+import com.jeffsieu.tasktracker.ChannelOverviewFragment;
 import com.jeffsieu.tasktracker.DashboardFragment;
 import com.jeffsieu.tasktracker.R;
 import com.jeffsieu.tasktracker.TagsFragment;
@@ -37,57 +43,68 @@ import com.jeffsieu.tasktracker.utils.DatabaseUtils;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, GoogleApiClient.OnConnectionFailedListener{
-	private static String TAG = MainActivity.class.getName();
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, GoogleApiClient.OnConnectionFailedListener {
+    private static String TAG = MainActivity.class.getName();
     public static final int CREATE_NEW_TASK = 0;
-	public static final int EDIT_TASK = 1;
-	public static MainActivity sReference;
+    public static final int EDIT_TASK = 1;
+    public static MainActivity sReference;
 
     public static List<Task> tasks = new ArrayList<>();
-	public static List<String> taskKeys = new ArrayList<>();
+    public static List<String> taskKeys = new ArrayList<>();
+    public static List<Channel> channels = new ArrayList<>();
+    public static List<String> channelKeys = new ArrayList<>();
     private static final DashboardFragment dashboardFragment = new DashboardFragment();
     private static final TagsFragment tagsFragment = new TagsFragment();
+    private static final ChannelOverviewFragment channelOverviewFragment = ChannelOverviewFragment.newInstance();
 
-	private GoogleApiClient mGoogleApiClient;
+    private static State currentState = State.DASHBOARD;
+
+    private GoogleApiClient mGoogleApiClient;
     private NavigationView navigationView;
-	private FloatingActionButton mFab;
-	public FirebaseAuth mAuth;
-	public FirebaseAuth.AuthStateListener mAuthListener;
+    private FloatingActionButton mFab;
+    public FirebaseAuth mAuth;
+    public FirebaseAuth.AuthStateListener mAuthListener;
 
-	private SubMenu tagSubMenu;
+    private static String UID;
+
+    private SubMenu tagSubMenu;
+
+    public enum State {
+        DASHBOARD, CHANNELS
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-		sReference = this;
+        sReference = this;
         super.onCreate(savedInstanceState);
 
-		mAuth = FirebaseAuth.getInstance();
-		mAuthListener = new FirebaseAuth.AuthStateListener() {
-			@Override
-			public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-				FirebaseUser user = firebaseAuth.getCurrentUser();
-				if (user != null) {
-					// User is signed in
-					Log.d("Log", "onAuthStateChanged:signed_in:" + user.getUid());
-				} else {
-					// User is signed out
-					Log.d("Log", "onAuthStateChanged:signed_out");
-				}
-				// ...
-			}
-		};
-		// Configure sign-in to request the user's ID, email address, and basic
-		// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
-		GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-				.requestIdToken(getString(R.string.default_web_client_id))
-				.requestEmail()
-				.build();
-		// Build a GoogleApiClient with access to the Google Sign-In API and the
-		// options specified by gso.
-		mGoogleApiClient = new GoogleApiClient.Builder(this)
-				.enableAutoManage(this, this)
-				.addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-				.build();
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d("Log", "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d("Log", "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
 
         setContentView(R.layout.activity_main);
         final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -98,11 +115,18 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setSupportActionBar(toolbar);
 
         mFab = (FloatingActionButton) findViewById(R.id.fab);
-		mFab.setOnClickListener(new View.OnClickListener() {
+        mFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, EditTaskActivity.class);
-                startActivityForResult(intent, CREATE_NEW_TASK);
+                switch (currentState) {
+                    case DASHBOARD:
+                        Intent intent = new Intent(getApplicationContext(), EditTaskActivity.class);
+                        startActivityForResult(intent, CREATE_NEW_TASK);
+                        break;
+                    case CHANNELS:
+                        showAddChannelDialog();
+                        break;
+                }
             }
         });
 
@@ -114,50 +138,53 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-		navigationView.getMenu().getItem(0).setChecked(true);
-		onNavigationItemSelected(navigationView.getMenu().getItem(0));
+        navigationView.getMenu().getItem(0).setChecked(true);
+        onNavigationItemSelected(navigationView.getMenu().getItem(0));
 
         if (savedInstanceState == null) {
-			FirebaseDatabase.getInstance().setPersistenceEnabled(true);
-			final OptionalPendingResult<GoogleSignInResult> pendingResult = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
+            FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+            final OptionalPendingResult<GoogleSignInResult> pendingResult = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
 
-			if (pendingResult.isDone()) {
-				// There's immediate result available.
-				Log.d(TAG, "Immediate");
-				silentSignIn(pendingResult.get());
-			} else {
-				// There's no immediate result ready, displays some progress indicator and waits for the
-				// async callback.
-				pendingResult.setResultCallback(new ResultCallback<GoogleSignInResult>() {
-					@Override
-					public void onResult(@NonNull GoogleSignInResult result) {
-						Log.d(TAG, "Not immediate");
-						silentSignIn(result);
-					}
-				});
-			}
-		}
-		tagSubMenu = navigationView.getMenu().findItem(R.id.nav_group_tags).getSubMenu();
+            if (pendingResult.isDone()) {
+                // There's immediate result available.
+                Log.d(TAG, "Immediate");
+                silentSignIn(pendingResult.get());
+            } else {
+                // There's no immediate result ready, displays some progress indicator and waits for the
+                // async callback.
+                pendingResult.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+                    @Override
+                    public void onResult(@NonNull GoogleSignInResult result) {
+                        Log.d(TAG, "Not immediate");
+                        silentSignIn(result);
+                    }
+                });
+            }
+        }
+        tagSubMenu = navigationView.getMenu().findItem(R.id.nav_group_tags).getSubMenu();
+
+        channels.add(new Channel("Testing", "1002i", getUID()));
+        channelKeys.add("iov");
     }
 
-	@Override
-	public void onStart() {
-		super.onStart();
-		mAuth.addAuthStateListener(mAuthListener);
-	}
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
 
-	@Override
-	public void onStop() {
-		super.onStop();
-		if (mAuthListener != null) {
-			mAuth.removeAuthStateListener(mAuthListener);
-		}
-	}
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
 
-	private void silentSignIn(GoogleSignInResult result) {
-		DatabaseUtils.handleSignInResult(result, this);
-		//Log.d(TAG, result.getSignInAccount().getDisplayName());
-	}
+    private void silentSignIn(GoogleSignInResult result) {
+        DatabaseUtils.handleSignInResult(result, this);
+        //Log.d(TAG, result.getSignInAccount().getDisplayName());
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -166,8 +193,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             // Make sure the request was successful
             if (resultCode == RESULT_OK) {
                 Task task = data.getParcelableExtra("task");
-				DatabaseUtils.pushTask(task);
+                DatabaseUtils.pushTask(task);
             }
+        }
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == DatabaseUtils.RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            DatabaseUtils.handleSignInResult(result, this);
         }
     }
 
@@ -188,10 +220,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         int id = item.getItemId();
 
         if (id == R.id.nav_dashboard) {
+            currentState = State.DASHBOARD;
             getSupportFragmentManager().beginTransaction().replace(R.id.activity_main_fragment, dashboardFragment).commit();
-
         } else if (id == R.id.nav_settings) {
             startActivity(new Intent(this, SettingsActivity.class));
+        } else if (id == R.id.nav_channels) {
+            currentState = State.CHANNELS;
+            getSupportFragmentManager().beginTransaction().replace(R.id.activity_main_fragment, channelOverviewFragment).commit();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -199,34 +234,74 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return true;
     }
 
-	@Override
-	public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
-	}
+    }
 
-	private void addTag(String name){
-		MenuItem tag = tagSubMenu.add(name);
-		tag.setIcon(R.drawable.label);
-		tag.setCheckable(true);
-	}
+    private void addTag(String name) {
+        MenuItem tag = tagSubMenu.add(name);
+        tag.setIcon(R.drawable.label);
+        tag.setCheckable(true);
+    }
 
-	public void showFab() {
-		mFab.show();
-	}
+    public void showFab() {
+        mFab.show();
+    }
 
     public DashboardFragment getDashboardFragment() {
         return dashboardFragment;
     }
 
-	public GoogleApiClient getGoogleApiClient() {
-		return mGoogleApiClient;
-	}
+    public GoogleApiClient getGoogleApiClient() {
+        return mGoogleApiClient;
+    }
 
-	public static int dpToPx(int dp) {
-		return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
-	}
+    public static int dpToPx(int dp) {
+        return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
+    }
 
-	public static int pxToDp(int px) {
-		return (int) (px / Resources.getSystem().getDisplayMetrics().density);
-	}
+    public static int pxToDp(int px) {
+        return (int) (px / Resources.getSystem().getDisplayMetrics().density);
+    }
+
+    private void showAddChannelDialog() {
+        LayoutInflater li = getLayoutInflater();
+        View promptsView = li.inflate(R.layout.dialog_add_channel, null);
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                this);
+
+        // set prompts.xml to alertdialog builder
+        alertDialogBuilder.setView(promptsView);
+
+        final EditText userInput = (EditText) promptsView
+                .findViewById(R.id.add_channel_name);
+
+        // set dialog message
+        alertDialogBuilder
+                .setCancelable(false)
+                .setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                            }
+                        })
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    public static String getUID() {
+        return UID;
+    }
+
+    public static void setUID(String UID) {
+        MainActivity.UID = UID;
+    }
 }
